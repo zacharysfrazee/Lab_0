367ed86bf299c9dd9b9d791508797ca10419eaa0
zacharysfrazee <zach@trailsendrepair.com>
2016-01-28 13:24:48 -0700

    New Class

---
+++ huffmanTree.java
@@ -0,0 +1,18 @@
+// tree.java
+// demonstrates binary tree
+// to run this program: C>java TreeApp
+
+import java.io.*;
+import java.util.*; // for Stack class
+
+class huffmanTree{
+	//Instance Variables
+	
+	//Constructor
+	public huffmanTree(){
+		
+	}
+	
+	//Methods
+}
+	
