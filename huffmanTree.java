d89542c25a380ce6f429aa5778c536c8e1200228
noyouaretheman <noyouaretheman@gmail.com>
2016-01-28 14:07:43 -0700

    implemented makeFrequencyTable method.

--- huffmanTree.java
+++ huffmanTree.java
@@ -20,7 +20,7 @@ class huffmanTree{
 	
 	//Constructor
 	public huffmanTree(String input){
-		self.input = input;
+		self.input = input.toUppercase();
 
 		//initialize the frequency table
 		for(int i = 0; i < maxSize; i++){
@@ -42,7 +42,26 @@ class huffmanTree{
 	public void decode(){}
 	
 	//Private Methods
-	private void makeFreqTable(){}
+	private void makeFreqTable(){
+		char currentChar;
+		int index; 
+		for(int i = 0; i < input.length(); i++){
+			currentChar = input.charAt(i);
+			if(currentChar.equals(' ')){
+				index = (int)currentChar - 6; //subtract 6 to get space (32) to an index of 26
+			}
+			else if(currentChar.equals('\n')){
+				index = (int)currentChar + 17; //add 17 to get lf (10) to an index of 27
+			}
+			else{
+
+			index = (int)currentChar - 65; //ascii valuve for A is 65. convert char to ascii then 
+										   //subtract the value of A to get an index for the frequency table
+			freqTable[index]++;
+			}
+		}
+	}
+
 	private void queueTree(){}
 	private void makeHuffmanTree(){}
 	
